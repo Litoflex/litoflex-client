39fbba7ff17a51aacb73259f0a68517f6353bcf9
pupkin-afk <ma5400026@gmail.com>
2026-01-12 18:47:34 +0200

    sadfdg

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	// остальные настройки
+};
+
+module.exports = nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
